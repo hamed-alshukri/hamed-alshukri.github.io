032d257065aa4685d3e4a49a43b9d9c2cbcbe9f2
Unique Creativity <uniquecreativity.net@gmail.com>
2016-07-08 13:02:17 +0400

    Convert indentation to tabs

--- Invent2016-WebDesign/js/main.js
+++ Invent2016-WebDesign/js/main.js
@@ -1,63 +1,63 @@
 ﻿var $j = jQuery.noConflict();
 
 $j(document).ready(function () {
-    "use strict"
+	"use strict"
 
-    initFlexSlider();
-    circleHover();
-    animateScroll();
+	initFlexSlider();
+	circleHover();
+	animateScroll();
 })
 
 function initFlexSlider() {
-    "use strict"
+	"use strict"
 
-    $j('.flexslider').flexslider({
-        animationLoop: true,
-        controlNav: false,
-        useCSS: false,
-        pauseOnAction: true,
-        pauseOnHover: true,
-        slideshow: true,
-        animation: 'slides',
-        prevText: "<img src='img/arrow-left.png' alt='Previous Slide' />",
-        nextText: "<img src='img/arrow-right.png' alt='Next Slide' />",
-        animationSpeed: 600,
-        slideshowSpeed: 8000,
-    });
+	$j('.flexslider').flexslider({
+		animationLoop: true,
+		controlNav: false,
+		useCSS: false,
+		pauseOnAction: true,
+		pauseOnHover: true,
+		slideshow: true,
+		animation: 'slides',
+		prevText: "<img src='img/arrow-left.png' alt='Previous Slide' />",
+		nextText: "<img src='img/arrow-right.png' alt='Next Slide' />",
+		animationSpeed: 600,
+		slideshowSpeed: 8000,
+	});
 
-    $j('.flex-direction-nav a').click(function (e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        e.stopPropagation();
-    });
+	$j('.flex-direction-nav a').click(function (e) {
+		e.preventDefault();
+		e.stopImmediatePropagation();
+		e.stopPropagation();
+	});
 }
 
 function circleHover() {
-    "use strict"
+	"use strict"
 
-    $j('#overview .overview-icon').hover(
-        function circleMouseEnter() {
-            var circle = $j(this).attr('Id');
-            $j("#inner-circle").addClass(circle + " hover");
-        },
-        function circleMouseExit() {
-            var circle = $j(this).attr('Id');
-            $j("#inner-circle").removeClass(circle + " hover");
-        });
+	$j('#overview .overview-icon').hover(
+		function circleMouseEnter() {
+			var circle = $j(this).attr('Id');
+			$j("#inner-circle").addClass(circle + " hover");
+		},
+		function circleMouseExit() {
+			var circle = $j(this).attr('Id');
+			$j("#inner-circle").removeClass(circle + " hover");
+		});
 }
 
 function animateScroll() {
-    "use strict";
+	"use strict";
 
-    $j(document).on('click', '#back-to-top', function (e) {
-        e.preventDefault();
+	$j(document).on('click', '#back-to-top', function (e) {
+		e.preventDefault();
 
-        $j('body,html').animate({ scrollTop: 0 }, $j(window).scrollTop() / 3, 'swing');
-    });
-    $j(document).on('click', '.animate-scroll', function (e) {
-        e.preventDefault();
-        var elementClass = $j(this).attr('href');
-        $j('body,html').animate({ scrollTop: $j(elementClass).offset().top }, 2000, 'swing');
-    });
+		$j('body,html').animate({ scrollTop: 0 }, $j(window).scrollTop() / 3, 'swing');
+	});
+	$j(document).on('click', '.animate-scroll', function (e) {
+		e.preventDefault();
+		var elementClass = $j(this).attr('href');
+		$j('body,html').animate({ scrollTop: $j(elementClass).offset().top }, 2000, 'swing');
+	});
 }
 
