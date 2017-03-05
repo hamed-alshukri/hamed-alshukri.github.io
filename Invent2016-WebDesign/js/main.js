63141c4620e7f56b798d50f8684ce991910f9bef
Unique Creativity <uniquecreativity.net@gmail.com>
2017-03-05 22:09:34 +0400

    Invent20106-WebDesign * revert back  to un-minified version of JS script due to minification errors

--- Invent2016-WebDesign/js/main.js
+++ Invent2016-WebDesign/js/main.js
@@ -1,1 +1,57 @@
-﻿var $j=jQuery.noConflict();$j(document).ready(function (){"use strict"initFlexSlider();circleHover();animateScroll();})function initFlexSlider(){"use strict"$j('.flexslider').flexslider({animationLoop: true,controlNav: false,useCSS: false,pauseOnAction: true,pauseOnHover: true,slideshow: true,animation: 'slides',prevText: "<img src='img/arrow-left.png' alt='Previous Slide'/>",nextText: "<img src='img/arrow-right.png' alt='Next Slide'/>",animationSpeed: 600,slideshowSpeed: 8000,});$j('.flex-direction-nav a').click(function (e){e.preventDefault();e.stopImmediatePropagation();e.stopPropagation();});}function circleHover(){"use strict"$j('#overview .overview-icon').hover(function circleMouseEnter(){var circle=$j(this).attr('Id');$j("#inner-circle").addClass(circle + " hover");},function circleMouseExit(){var circle=$j(this).attr('Id');$j("#inner-circle").removeClass(circle + " hover");});}function animateScroll(){"use strict";$j(document).on('click', '#back-to-top', function (e){e.preventDefault();$j('body,html').animate({scrollTop: 0}, $j(window).scrollTop() / 3, 'swing');});$j(document).on('click', '.animate-scroll', function (e){e.preventDefault();var elementClass=$j(this).attr('href');$j('body,html').animate({scrollTop: $j(elementClass).offset().top}, 2000, 'swing');});}
+
+var $j = jQuery.noConflict();
+$j(document).ready(function() {
+    "use strict"
+    initFlexSlider();
+    circleHover();
+    animateScroll();
+})
+function initFlexSlider() {
+    "use strict"
+    $j('.flexslider').flexslider({
+        animationLoop: true,
+        controlNav: false,
+        useCSS: false,
+        pauseOnAction: true,
+        pauseOnHover: true,
+        slideshow: true,
+        animation: 'slides',
+        prevText: "<img src='img/arrow-left.png' alt='Previous Slide'/>",
+        nextText: "<img src='img/arrow-right.png' alt='Next Slide'/>",
+        animationSpeed: 600,
+        slideshowSpeed: 8000,
+    });
+    $j('.flex-direction-nav a').click(function(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        e.stopPropagation();
+    });
+}
+
+function circleHover() {
+    "use strict"
+    $j('#overview .overview-icon').hover(function circleMouseEnter() {
+        var circle = $j(this).attr('Id');
+        $j("#inner-circle").addClass(circle + " hover");
+    }, function circleMouseExit() {
+        var circle = $j(this).attr('Id');
+        $j("#inner-circle").removeClass(circle + " hover");
+    });
+}
+
+function animateScroll() {
+    "use strict";
+    $j(document).on('click', '#back-to-top', function(e) {
+        e.preventDefault();
+        $j('body,html').animate({
+            scrollTop: 0
+        }, $j(window).scrollTop() / 3, 'swing');
+    });
+    $j(document).on('click', '.animate-scroll', function(e) {
+        e.preventDefault();
+        var elementClass = $j(this).attr('href');
+        $j('body,html').animate({
+            scrollTop: $j(elementClass).offset().top
+        }, 2000, 'swing');
+    });
+}
